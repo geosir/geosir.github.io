9993c7589c50887fd1b01df79e9ed4088e058ef6
George Moe <george@george.moe>
2016-04-08 23:16:13 -0500

    Updated website.

--- meta/ga.js
+++ meta/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-55205533-9', 'auto');
-ga('send', 'pageview');
+ga('create', 'UA-55205533-9', {'allowAnchor': true});
+ga('send', 'pageview', { 'page': location.pathname + location.search + location.hash});
